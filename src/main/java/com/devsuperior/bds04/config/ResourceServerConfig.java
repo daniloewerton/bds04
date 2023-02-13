107fa5e0c2dbc21a5e3ae435f53c5e3574d8539d
Danilo Ewerton <danilo.ewe@gmail.com>
2023-02-13 18:56:22 -0300

    Correção no serviço de login

---
+++ src/main/java/com/devsuperior/bds04/config/ResourceServerConfig.java
@@ -0,0 +1,39 @@
+package com.devsuperior.bds04.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Autowired
+    private Environment env;
+
+    private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
+
+    private static final String[] CLIENT_OR_ADMIN = { "/cities/**", "/events/**" };
+
+    private static final String[] ADMIN = { "/users/**" };
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        if(Arrays.asList(env.getActiveProfiles()).contains("test")) {
+            http.headers().frameOptions().disable();
+        }
+
+        http.authorizeRequests()
+                .antMatchers(PUBLIC).permitAll()
+                .antMatchers(HttpMethod.GET, CLIENT_OR_ADMIN).permitAll()
+                .antMatchers(CLIENT_OR_ADMIN).hasAnyRole("CLIENT", "ADMIN")
+                .antMatchers(ADMIN).hasRole("ADMIN")
+                .anyRequest().authenticated();
+    }
+}
--- src/main/java/com/devsuperior/bds04/config/WebSecurityConfig.java
+++ src/main/java/com/devsuperior/bds04/config/WebSecurityConfig.java
@@ -3,56 +3,23 @@ package com.devsuperior.bds04.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import java.util.Arrays;
-
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private Environment env;
 
     @Autowired
     private UserDetailsService userDetailsService;
 
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
-
-    private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
-
-    private static final String[] CLIENT_OR_ADMIN = { "/cities/**", "/events/**" };
-
-    private static final String[] ADMIN = { "/users/**" };
-
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        if(Arrays.asList(env.getActiveProfiles()).contains("test")) {
-            http.headers().frameOptions().disable();
-        }
-
-        http.authorizeRequests()
-                .antMatchers(PUBLIC).permitAll()
-                .antMatchers(HttpMethod.GET, CLIENT_OR_ADMIN).permitAll()
-                .antMatchers(CLIENT_OR_ADMIN).hasAnyRole("CLIENT", "ADMIN")
-                .antMatchers(ADMIN).hasRole("ADMIN")
-                .anyRequest().authenticated();
-    }
-
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/actuator/**");
-    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -63,5 +30,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/actuator/**");
     }
 }
